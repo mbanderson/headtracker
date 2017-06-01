dd05e2c31599ee48623aab21dd9ba1aec1edcd70
mbanderson <mbanderson@ucla.edu>
2017-05-31 23:21:31 -0700

    Changed omega values to rad/s. Added quaternion integration.

--- src/tracker/AxisAngVals.m
+++ src/tracker/AxisAngVals.m
@@ -6,7 +6,7 @@ function [w] = AxisAngVals(axis,ts)
 %       axis - (int) 1: x-axis, 2: y-axis, 3: z-axis
 %       ts - (optional)(Nx1 float vec) time domain
 %   OUTPUTS:
-%       w - (Nx3 float vec) angular velocity values
+%       w - (Nx3 float vec) angular velocity values RAD/S
 %
 
 narginchk(1,2);
@@ -16,10 +16,10 @@ if nargin < 2
     tf = 100;
     ts = t0:dt:tf; % sim time, s
 end
-w = zeros(numel(ts),3); % deg/s
+w = zeros(numel(ts),3); % RAD/S
 
 % Maximum angular velocity
-ysat = 45; % max ang vel, deg/s
+ysat = 0.5; % max ang vel, RAD/S
 T = 2.5; % rotation period, s
 
 % Generate model values and assign to specified axis
---
+++ src/tracker/QuatIntegration.m
@@ -0,0 +1,21 @@
+function [q] = QuatIntegration(q0,w,dt)
+%QUATINTEGRATION Performs numerical integration of input quaternion.
+%   The subsequent quaternion resulting from angular velocity w given by:
+%       q_{t+1} = q_t + Aq_tdt
+%   INPUTS:
+%       q0 - (4x1 float vec) starting quaternion
+%       w - (3x1 float vec) angular velocity
+%       dt - (float) time discretization step
+%   OUTPUTS:
+%       q - (4x1 float vec) resulting quaternion
+%
+
+omega = [0, -w(3), w(2);
+         w(3), 0, -w(1);
+        -w(2), w(1), 0];
+A = 1/2*[0, -w';
+         w, omega];
+q = q0 + A*q0*dt;
+q = q / norm(q);
+
+end
